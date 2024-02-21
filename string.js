3de8bea98a131e7537f1135dcdf808c8dc71e80d
keni <kenikakadiya@gmail.com>
2024-02-21 09:03:01 +0530

    javascript

---
+++ string.js
@@ -0,0 +1,19 @@
+let x = " java script";
+console.log(x.length);
+let y=x.toUpperCase();
+let z=x.toLowerCase();
+console.log(y);
+console.log(z);
+console.log(x.split('j'));
+
+
+let k = "            keni kakadiya            ";
+console.log(k.trim());
+console.log(k.trimEnd());
+console.log(k.trimStart());
+
+
+let r = "158427";
+console.log(r.padStart(12,'x'));
+console.log(r.padEnd(12,'x'));
+
