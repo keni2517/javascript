3a4c268cf56ba3a3500e3dc0860c04d32a5145b9
keni <kenikakadiya@gmail.com>
2024-02-27 07:56:49 +0530

    javascript

---
+++ date.js
@@ -0,0 +1,30 @@
+const k = new Date();
+// const k = new Date("October 13, 2014 11:13:00"); 
+
+console.log(k);
+
+//k.setFullYear(2025);
+//k.setMonth(8);
+//k.setDate(85);
+//k.setDay(6);
+//k.setHours(5);
+//k.setMinutes(14);
+//k.setSeconds(850);
+//k.setMilliSeconds(100000000);
+//k.setTime(4);
+
+k.setUTCDate(26);
+
+console.log(k);
+
+// console.log(k.getFullYear(2025));
+// console.log(k.getMonth(8));
+// console.log(k.getDate(85));
+// console.log(k.setDay(6));
+// console.log(k.getHours(5));
+// console.log(k.getMinutes(14));
+// console.log(k.getSeconds(850));
+// console.log(k.setMilliSeconds(100000000));
+// console.log(k.getTime(4));
+
+// console.log(k);
