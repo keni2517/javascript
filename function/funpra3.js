60662bd79223b72e2a321b160018aa9d37987ac6
keni <kenikakadiya@gmail.com>
2024-02-28 09:16:04 +0530

    javascript

--- function/funpra3.js
+++ function/funpra3.js
@@ -5,13 +5,41 @@
 
 
 
-function uppercase(str) {
-    var array1 = str.split(' ');
-    console.log(array1);
-    var newarray1 = [];
-    for (var x = 0; x < array1.length; x++) {
-      newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
+// function uppercase(str) {
+//     var array1 = str.split(' ');
+//     console.log(array1);
+//     var newarray1 = [];
+//     for (var x = 0; x < array1.length; x++) {
+//       newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
+//     }
+//     return newarray1.join(' ');
+//   }
+//   console.log(uppercase("the quick brown fox"));
+
+
+
+  {
+
+    function LongestWord (words){
+  
+      let New_word = words.split(' ');
+      let wordLength = [];
+    
+      for(let i = 0 ; i < New_word.length ; i++)
+      {
+        wordLength.push(New_word[i].slice(0).length);
+      }
+  
+      // let total = Math.max.apply(null,wordLength);
+    
+      let findInd = New_word.findIndex((val)=>{
+        return (Math.max.apply(null,wordLength) === val.length) ; //2
+      });
+  
+      return New_word[findInd];
+  
     }
-    return newarray1.join(' ');
+    
+    let InWord = 'Web Development Tutorial' ;
+    console.log(LongestWord(InWord));
   }
-  console.log(uppercase("the quick brown fox")); 
