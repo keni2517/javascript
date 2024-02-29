3592e819630d6d2a88bf3dba664c122e91566d61
keni <kenikakadiya@gmail.com>
2024-02-29 07:54:31 +0530

    javascript

---
+++ object.js
@@ -0,0 +1,22 @@
+let person={
+    firstname :"keni",
+    lastname : "kakadiya",
+    email: "kenikakadita@gmai.com"
+}
+
+//console.log(person);
+//console.log(person.firstname);
+//console.log(person["lastname"]);
+
+person.address = {
+    line : "208,city center",
+    city:"surat",
+    state:"gujarat"
+}
+//console.log(person);
+
+person.hobies = ["reading, dancing, music"]
+//console.log(person);
+console.log(person["hobies"]);
+//console.log(person["address"]["city"]);
+
