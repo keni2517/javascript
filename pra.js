4c700c4350f13dbda37e58a2007a09d420d83bdd
keni <kenikakadiya@gmail.com>
2024-03-12 09:18:18 +0530

    javacsript

---
+++ pra.js
@@ -0,0 +1,57 @@
+// var a = "Skillqode";
+// var result = a.substring(2,4);
+// console.log(result);
+
+
+// var a = [1,2,3,4,5];
+// var result = a.slice(2,4);
+// console.log(result);
+
+
+
+// console.log(isNaN === isNaN);
+
+
+// console.log(typeof(NaN));
+
+
+//var examp = ({a,b,c}) => {
+    //console.log(b*2,a*5,c*2);
+//};
+//examp ({a:0 , b:1 , c:2});
+
+// var a = "hello";
+// console.log(a.charCodeAt(4));
+// var sum = 0;
+// for (var i = 0; i < a.length; i++) {
+//     sum +=(a.charCodeAt(i) - 'a'.charCodeAt(0));
+// }
+
+// console.log(sum);
+
+// let s = "00000001111111";
+// let l = 0 , r=s.length-1 ,ans =-1;
+// while(l<=r){
+//     var mid = Math.floor((1+r)/2);
+//     if(s[mid] =='1'){
+//         ans = mid;
+//         r=mid-1;
+//     }
+//     else
+//     l=mid+1;
+// }
+// console.log(ans);
+
+const obj1 = { name:"hello" ,Age:16};
+const obj2 = { name:"hello" ,Age:16};
+
+console.log(obj1 === obj2);
+
+// const obj1 = { first:20 , second:38 ,first:50};
+// console.log(obj1);
+
+// var a =1;
+// if(a!=null)
+//     console.log(1);
+// else
+//     console.log(0);
