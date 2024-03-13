13fa218f83a02a7b4bd5bd65b174c43115e9bead
keni <kenikakadiya@gmail.com>
2024-03-13 18:08:40 +0530

    javascript

--- pra.js
+++ pra.js
@@ -1,57 +1,30 @@
-// var a = "Skillqode";
-// var result = a.substring(2,4);
-// console.log(result);
 
-
-// var a = [1,2,3,4,5];
-// var result = a.slice(2,4);
-// console.log(result);
-
-
-
-// console.log(isNaN === isNaN);
-
-
-// console.log(typeof(NaN));
-
-
-//var examp = ({a,b,c}) => {
-    //console.log(b*2,a*5,c*2);
-//};
-//examp ({a:0 , b:1 , c:2});
 
 // var a = "hello";
 // console.log(a.charCodeAt(4));
 // var sum = 0;
-// for (var i = 0; i < a.length; i++) {
-//     sum +=(a.charCodeAt(i) - 'a'.charCodeAt(0));
+//  for (var i = 0; i < a.length; i++) {
+//          sum +=(a.charCodeAt(i) - 'a'.charCodeAt(0));
 // }
 
 // console.log(sum);
-
-// let s = "00000001111111";
-// let l = 0 , r=s.length-1 ,ans =-1;
-// while(l<=r){
-//     var mid = Math.floor((1+r)/2);
-//     if(s[mid] =='1'){
-//         ans = mid;
-//         r=mid-1;
-//     }
-//     else
-//     l=mid+1;
-// }
-// console.log(ans);
 
 const obj1 = { name:"hello" ,Age:16};
 const obj2 = { name:"hello" ,Age:16};
 
 console.log(obj1 === obj2);
 
-// const obj1 = { first:20 , second:38 ,first:50};
-// console.log(obj1);
+function show(...args){
+    let sum =0;
+    for(let i in args)
+    sum+=args[i];
+console.log(sum);
+}
+show(40,11,5);
 
-// var a =1;
-// if(a!=null)
-//     console.log(1);
-// else
-//     console.log(0);
+
+const numbers = [45, 4, 9, 16, 25];
+const firstIndex = numbers.findIndex(function (val){
+			return val > 18;
+});
+console.log(firstIndex);
